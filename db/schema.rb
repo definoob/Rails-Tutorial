774e59a5d4290343ce52cc9f9842965c8226bfa0
definoob <noahjun511@gmail.com>
2021-12-01 22:40:00 +0800

    Using Concern

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_122131) do
+ActiveRecord::Schema.define(version: 2021_12_01_140906) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
